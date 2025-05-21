b0b7862a95fb71cdc6f08595f2e17f466bd21e9d
Manish Singh <officialmanishsingh03@gmail.com>
2025-05-21 13:12:04 +0530

    Update Hero.jsx

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,9 +26,9 @@ const Hero = () => {
         <h2>About Me</h2>
         <p>I'm Manish Singh, a frontend developer skilled in HTML, CSS, JavaScript, and React.js. I enjoy building responsive, user-friendly web interfaces and constantly improving my skills through real-world projects.</p>
         <div className="social-icons">
-          <a href="https://linkedin.com" target="_blank"><FaLinkedin /></a>
-          <a href="https://github.com" target="_blank"><FaGithub /></a>
-          <a href="https://instagram.com" target="_blank"><FaInstagram /></a>
+          <a href="https://www.linkedin.com/in/manish-singh-916a9b276?utm_source=share&utm_campaign=share_via&utm_content=profile&utm_medium=android_app" target="_blank"><FaLinkedin /></a>
+          <a href="https://github.com/officialmanish03" target="_blank"><FaGithub /></a>
+          <a href="https://www.instagram.com/officialmanish03?igsh=MTczN3BoMjJ3YmxkeQ==" target="_blank"><FaInstagram /></a>
         </div>    
       </div>
 
